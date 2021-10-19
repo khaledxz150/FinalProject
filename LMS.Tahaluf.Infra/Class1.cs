c243eb98984eaf381d038815c92bb7becd9c52a7
khaledxz150 <khaledxz150@hotmail.com>
2021-10-19 17:58:16 +0300

    update

--- LMS.Tahaluf.Infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LMS.Tahaluf.Infra
-{
-    public class Class1
-    {
-    }
-}
---
+++ LMS/LMS.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LMS.Infra
+{
+    public class Class1
+    {
+    }
+}
