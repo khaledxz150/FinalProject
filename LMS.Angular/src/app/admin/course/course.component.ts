e3b57470639d41d3658e290e936bca7cef680a72
shahedmalkawi <shahedmalkawi19@gmail.com>
2021-11-08 19:15:27 +0200

    .

--- LMS.Angular/src/app/admin/course/course.component.ts
+++ LMS.Angular/src/app/admin/course/course.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngleDoubleRight, faShoppingCart, faHeart, faQuoteRight, faStar, faUser, faBook, faTag, faChartLine, faCalendar, faDollarSign, faPercentage} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-course',
@@ -7,6 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+
+  // Icons
+  faAngleDoubleRight = faAngleDoubleRight
+  faShoppingCart = faShoppingCart
+  faHeart = faHeart
+  faQuoteRight = faQuoteRight
+  faStar = faStar
+  faUser = faUser
+  faBook = faBook
+  faTag = faTag
+  faChartLine = faChartLine
+  faCalendar = faCalendar
+  faDollarSign = faDollarSign
+  faPercentage = faPercentage
   constructor() { }
 
   ngOnInit(): void {
