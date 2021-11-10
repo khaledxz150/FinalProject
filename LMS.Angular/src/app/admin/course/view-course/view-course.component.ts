19f652d84d5a39b33bbbf64cd396cd77bf239ffc
shahedmalkawi <shahedmalkawi19@gmail.com>
2021-11-10 13:28:31 +0200

    .

---
+++ LMS.Angular/src/app/admin/course/view-course/view-course.component.ts
@@ -0,0 +1,49 @@
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CourseService } from 'src/app/Service/course.service';
+
+@Component({
+  selector: 'app-view-course',
+  templateUrl: './view-course.component.html',
+  styleUrls: ['./view-course.component.css']
+})
+export class ViewCourseComponent implements OnInit {
+
+  @Input() courseId:number |undefined;
+  @Input() courseName:string ="";
+  @Input() courseDescripction:string|undefined
+  @Input() coursePrice:number | undefined;
+  @Input() image:string | undefined;
+  @Input() typeName:string | undefined;
+  @Input() passMark:number|undefined
+  @Input() levelName:string|undefined
+  @Input() categoryName:string|undefined
+  @Input() tagName:string|undefined
+
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public courseService: CourseService) { }
+
+  ngOnInit(): void {
+
+     if (this.data) {
+      this.courseId = this.data.courseId;
+      this.coursePrice = this.data.coursePrice;
+      this.courseName = this.data.courseName;
+      this.courseDescripction = this.data.courseDescripction;
+      this.image = this.data.image;
+      this.typeName = this.data.typeName;
+      this.passMark = this.data.passMark;
+      this.levelName = this.data.levelName;
+      this.tagName = this.data.tagName;
+      this.categoryName = this.data.categoryName;
+    }
+  }
+
+
+  deleteCourse(){
+    console.log(this.courseId)
+    if(this.courseId)
+    this.courseService.deleteCourse(this.courseId);
+  }
+
+}
--- LMS.Angular/src/app/admin/sidebar/sidebar.component.ts
+++ LMS.Angular/src/app/admin/sidebar/sidebar.component.ts
@@ -6,20 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css','../../../assets/css/style1.css']
 })
 export class SidebarComponent implements OnInit {
- 
 
 
- constructor() { 
-   
+  sidebar:Element | null = null;
+   closeBtn:Element | null = null;
+   searchBtn:Element | null = null;
+
+  constructor() {
+
+   }
+   ngOnInit(): void {
+     this.searchBtn = document.querySelector(".bx-search");
+     this.closeBtn= document.querySelector("#btn");
+     this.sidebar = document.querySelector(".sidebar");
   }
-  ngOnInit(): void {
+
+  btnclicked(){
+    this.sidebar!.classList.toggle("open");
+    this.menuBtnChange();
   }
-   
- 
- 
- // following are the code to change sidebar button(optional)
- 
- 
+
+  // following are the code to change sidebar button(optional)
+  menuBtnChange(this: any) {
+   if(this.sidebar.classList.contains("open")){
+     this.closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   }else {
+     this.closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   }
+  }
 }
 
 
