69168c893798f34af989d21dccf9a282c5b77646
JasserAlshaer <49323288+JasserAlshaer@users.noreply.github.com>
2021-11-14 17:05:38 +0200

    first api code edit
    change Customer Controller , Repository,Service

--- LMS.Angular/src/app/trainee/trainee-routing.module.ts
+++ LMS.Angular/src/app/trainee/trainee-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CertificationComponent } from './certification/certification.component';
+import { ExamInfoComponent } from './exam-info/exam-info.component';
 import { MyLeaningComponent } from './my-leaning/my-leaning.component';
+import { MySectionsComponent } from './my-sections/my-sections.component';
 import { ProfileComponent } from './profile/profile.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { TaskInfoComponent } from './task-info/task-info.component';
 import { TraineeNavComponent } from './trainee-nav/trainee-nav.component';
+import { VideosComponent } from './videos/videos.component';
 
 const routes: Routes = [
   {
@@ -16,7 +23,31 @@ const routes: Routes = [
   {
     path:'learning',
     component:MyLeaningComponent
+  },{
+    path:'sections'
+    ,component:MySectionsComponent
+  },{
+    path:'questions',
+    component:QuestionsComponent
   }
+  ,{
+    path:'purchase',
+    component:PurchaseComponent
+  },{
+    path:'video',
+    component:VideosComponent
+  },{
+    path:'info',
+    component:ExamInfoComponent
+  },{
+    path:'task',
+    component:TaskInfoComponent
+  },{
+    path:'certificate',
+    component:CertificationComponent
+
+  }
+
 ];
 
 @NgModule({
