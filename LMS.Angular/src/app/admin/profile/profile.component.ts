78908a24fcdbc9ba1495b99c6021656631ae86b1
shahedmalkawi <shahedmalkawi19@gmail.com>
2021-11-17 20:46:22 +0200

    ..

--- LMS.Angular/src/app/admin/profile/profile.component.ts
+++ LMS.Angular/src/app/admin/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrainerService } from 'src/app/Service/trainer.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public trainerService: TrainerService) { }
 
   ngOnInit(): void {
+    let user:any = localStorage.getItem('user');
+
+    let trainerId = JSON.parse(user);
+     if(trainerId){
+      this.trainerService.ReturnEmployeeInfo(parseInt(trainerId.EmployeeId));
+
+    }
   }
 
 }
